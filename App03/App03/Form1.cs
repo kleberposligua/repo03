e1f4844796183810ddbf49676169ad2a4f6d2d81
kleberposligua <kleberposligua@hotmail.com>
2019-10-30 22:46:26 -0500

    Botón Hola

--- App03/App03/Form1.cs
+++ App03/App03/Form1.cs
@@ -16,5 +16,10 @@ namespace App03
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola");
+        }
     }
 }
